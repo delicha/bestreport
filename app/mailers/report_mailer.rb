1ff52dae7be9ffd7bf795fc5234a87c3b71b9867
provisionshohei <provisioninc@outlook.jp>
2021-04-29 18:44:29 +0900

    change to only sending text email

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,10 @@
 class ReportMailer < ApplicationMailer
-    default from: 'provisioninc@outlook.jp'
+    default from: 'iwakibestone@gmail.com'
 
     def creation_email(report)
         @report = report
-        mail(
-            subject: '個人指導報告書',
-            to: @report.student.email
-        )
+        mail(subject: '個人指導報告書', to: @report.student.email) do |format|
+            format.text #テキストメールを指定
+        end
     end
 end
