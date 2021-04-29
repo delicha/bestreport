d2598bcacc6842eeb1f6988ba3595d70cab5c41e
provisionshohei <provisioninc@outlook.jp>
2021-04-29 17:52:20 +0900

    email content

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,11 @@
 class ReportMailer < ApplicationMailer
-    default from: 'provisioninc@outlook.jp'
-    
+    default from: 'iwakibestone@gmail.com'
+
     def creation_email(report)
         @report = report
         mail(
-            subject: '報告書作成完了メール',
-            to: 'shoheiyamamoto919@gmail.com'
+            subject: '個人指導報告書',
+            to: '#{@report.email}'
         )
     end
 end
