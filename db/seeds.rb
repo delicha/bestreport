bb7f8feddf2784178a0a46fd929fa5c968a31c47
provisionshohei <provisioninc@outlook.jp>
2021-05-07 00:02:40 +0900

    add data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!(
-    name: '山本　昇平',
-    kana: 'ヤマモト　ショウヘイ',
+    name: '山本  昇平',
+    kana: 'ヤマモト  ショウヘイ',
     email: 'provisioninc@outlook.jp',
     subject: '英語',
     password: 'pass',
@@ -9,8 +9,8 @@ User.create!(
 )
 
 User.create!(
-    name: '川畑　歩美',
-    kana: 'カワバタ　アユミ',
+    name: '川畑  歩美',
+    kana: 'カワバタ  アユミ',
     email: 'ecckawabata@outlook.jp',
     subject: '英語 国語 数学',
     password: 'pass',
@@ -19,8 +19,8 @@ User.create!(
 )
 
 User.create!(
-    name: 'ピチクール　仁香',
-    kana: 'ピチクール　ニカ',
+    name: 'ピチクール  仁香',
+    kana: 'ピチクール  ニカ',
     email: 'eccnika@outlook.jp',
     subject: '英語',
     password: 'pass',
@@ -30,8 +30,8 @@ User.create!(
 
 Student.create!(
     name: '柳内  悠惺',
-    kana: 'ヤナイ　ユウセイ',
-    email: 'yanai@email.com',
+    kana: 'ヤナイ  ユウセイ',
+    email: 'shoheiyamamoto919@gmail.com',
     birthdate: '20070613',
     school: '平三中',
     memo: ''
@@ -40,10 +40,36 @@ Student.create!(
 Student.create!(
     name: '大野 心路',
     kana: 'オオノ ココロ',
-    email: 'shoheiyamamoto@hotmail.com',
+    email: 'iwakibestone2@gmail.com',
     birthdate: '20090401',
     school: '平三中',
     memo: ''
 )
 
+Student.create!(
+    name: '國友  美里',
+    kana: 'クニトモ  ミノリ',
+    email: 'iwakibestone3@gmail.com',
+    birthdate: '20041023',
+    school: '桜ヶ丘高',
+    memo: ''
+)
+
+Student.create!(
+    name: '新妻  乃々香',
+    kana: 'ニイツマ  ノノカ',
+    email: 'iwakibestone@gmail.com',
+    birthdate: '20050607',
+    school: '平三中',
+    memo: ''
+)
+
+Student.create!(
+    name: '松本  麻央',
+    kana: 'マツモト  マオ',
+    email: 'iwakibestone1@gmail.com',
+    birthdate: '2009/06/02',
+    school: '御厩小',
+    memo: ''
+)
 
