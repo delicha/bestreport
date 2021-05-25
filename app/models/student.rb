d45eb000e3e6cbfc69889d47e82acde038abd4d6
provisionshohei <provisioninc@outlook.jp>
2021-05-25 21:29:55 +0900

    delete uniqueness student email

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,7 @@ class Student < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX },
-                        uniqueness: { case_sensitive: false }
+                        # uniqueness: { case_sensitive: false }
     validates :school, presence: true, length: { maximum: 30 } 
     validates :birthdate, presence: true, length: { maximum: 20 }
 
