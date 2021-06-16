1c8f2e2e4c000ae275ba6bf08fbb7545094b5106
provisionshohei <provisioninc@outlook.jp>
2021-06-16 23:26:51 +0900

    change paginate css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,7 @@
 User.create!(
-    name: '山本　昇平',
-    kana: 'やまもと　しょうへい',
-    email: 'ya@email.com',
-    subject: '英語',
-    password: 'best',
-    password_confirmation: 'best',
-    admin: true
-)
-
-User.create!(
-    name: '川畑　歩美',
-    kana: 'かわばた　あゆみ',
-    email: 'ka@email.com',
-    subject: '英語　国語　数学',
-    password: 'best',
-    password_confirmation: 'best',
-    admin: true
-)
-
-User.create!(
-    name: 'ピチクール　仁香',
-    kana: 'ぴちくーる　にか',
-    email: 'ni@email.com',
-    subject: '英語',
-    password: 'best',
-    password_confirmation: 'best',
-    admin: true
-)
-
-User.create!(
-    name: '岑　正幸',
-    kana: 'みね　まさゆき',
-    email: 'mine@email.com',
-    subject: '英語',
-    password: 'pass',
-    password_confirmation: 'pass',
-    admin: false
-)
-
-User.create!(
-    name: '佐藤　美穂',
-    kana: 'さとう　みほ',
-    email: 'sato@email.com',
-    subject: '英語',
-    password: 'pass',
-    password_confirmation: 'pass',
-    admin: false
-)
-
-User.create!(
-    name: '齋藤　拓夢',
-    kana: 'さいとう　たくむ',
-    email: 'saito@email.com',
-    subject: '英語　数学　理科',
-    password: 'pass',
-    password_confirmation: 'pass',
-    admin: false
-)
-
-User.create!(
-    name: '湊　宥貴',
-    kana: 'みなと　ひろき',
-    email: 'minato@email.com',
+    name: '芦口　亜弓',
+    kana: 'あしぐち　あゆみ',
+    email: 'ashiguchi@email.com',
     subject: '英語　数学',
     password: 'pass',
     password_confirmation: 'pass',
@@ -79,9 +19,59 @@ User.create!(
 )
 
 User.create!(
-    name: '芦口　亜弓',
-    kana: 'あしぐち　あゆみ',
-    email: 'ashiguchi@email.com',
+    name: 'オーナー',
+    kana: 'おーなー',
+    email: 'owner@email.com',
+    subject: '英語',
+    password: 'best',
+    password_confirmation: 'best',
+    admin: true
+)
+
+User.create!(
+    name: '荻野　凌',
+    kana: 'おぎの　りょう',
+    email: 'ogino@email.com',
+    subject: '数学　英語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: '管理者',
+    kana: 'かんりしゃ',
+    email: 'best@email.com',
+    subject: '5教科',
+    password: 'best',
+    password_confirmation: 'best',
+    admin: true
+)
+
+User.create!(
+    name: '齋藤　拓夢',
+    kana: 'さいとう　たくむ',
+    email: 'saito@email.com',
+    subject: '英語　数学　理科',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: '佐藤　美穂',
+    kana: 'さとう　みほ',
+    email: 'sato@email.com',
+    subject: '英語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: '白江　五輝',
+    kana: 'しらえ　いつき',
+    email: 'shirae@email.com',
     subject: '英語　数学',
     password: 'pass',
     password_confirmation: 'pass',
@@ -99,16 +89,6 @@ User.create!(
 )
 
 User.create!(
-    name: '引地　良偉夢',
-    kana: 'ひきち　らいむ',
-    email: 'hikichi@email.com',
-    subject: '英語',
-    password: 'pass',
-    password_confirmation: 'pass',
-    admin: false
-)
-
-User.create!(
     name: '林　祐三',
     kana: 'はやし　ゆうぞう',
     email: 'hayashi@email.com',
@@ -119,9 +99,29 @@ User.create!(
 )
 
 User.create!(
-    name: '白江　五輝',
-    kana: 'シラエ　イツキ',
-    email: 'shirae@email.com',
+    name: '引地　良偉夢',
+    kana: 'ひきち　らいむ',
+    email: 'hikichi@email.com',
+    subject: '英語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: 'ピチクール　仁香',
+    kana: 'ぴちくーる　にか',
+    email: 'nika@email.com',
+    subject: '英語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: '湊　宥貴',
+    kana: 'みなと　ひろき',
+    email: 'minato@email.com',
     subject: '英語　数学',
     password: 'pass',
     password_confirmation: 'pass',
@@ -129,14 +129,37 @@ User.create!(
 )
 
 User.create!(
-    name: '荻野　凌',
-    kana: 'おぎの　りょう',
-    email: 'ogino@email.com',
-    subject: '数学　英語',
+    name: '岑　正幸',
+    kana: 'みね　まさゆき',
+    email: 'mine@email.com',
+    subject: '英語',
     password: 'pass',
     password_confirmation: 'pass',
     admin: false
 )
+
+User.create!(
+    name: '山本　昇平',
+    kana: 'やまもと　しょうへい',
+    email: 'yamamoto@email.com',
+    subject: '英語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+User.create!(
+    name: '和田　真弓',
+    kana: 'わだ　まゆみ',
+    email: 'wada@email.com',
+    subject: '英語　国語',
+    password: 'pass',
+    password_confirmation: 'pass',
+    admin: false
+)
+
+
+
 
 # Student.create!(
 #     name: '柳内  悠惺',
