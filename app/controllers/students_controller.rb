deafb40a168ff8538c13d14138d5a20d56c08235
provisionshohei <provisioninc@outlook.jp>
2021-06-17 18:51:30 +0900

    n1 student show

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,8 +18,11 @@ class StudentsController < ApplicationController
 
   def show
     @birth = cal_birth
-    @q = @student.reports.ransack(params[:q])
+    # @q = @student.reports.ransack(params[:q])
+    @q = Report.where(:user_id == @student).ransack(params[:q])
     @reports = @q.result(distinct: true).page(params[:page]).per(5).order('created_at DESC')
+    
+    
   end
 
   def new
